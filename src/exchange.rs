2b772d462c376faae9c2a23799b06828c96ce330
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-05-01 07:53:15 +0200

    feature(exchange): add `buy` and `sell` method to `Exchange`

--- src/exchange.rs
+++ src/exchange.rs
@@ -13,6 +13,28 @@ use serde::Deserialize;
 
 pub trait Exchange: Stream {
     fn order_book_subscribe(&self, symbol: &Symbol);
+
+    fn buy(&self, amount: f64, price: f64, wallet: Wallet) -> anyhow::Result<Wallet> {
+        // We are buying base token for quote token
+        let new_base_amount = wallet.base + amount;
+        let new_quote_amount = wallet.quote - (price * amount);
+        Ok(Wallet {
+            base: new_base_amount,
+            quote: new_quote_amount,
+        })
+    }
+
+    fn sell(&self, amount: f64, price: f64, wallet: Wallet) -> anyhow::Result<Wallet> {
+        // We are selling base token for quote token
+        let new_base_amount = wallet.base - amount;
+        let new_quote_amount = wallet.quote + (price * amount);
+        Ok(Wallet {
+            base: new_base_amount,
+            quote: new_quote_amount,
+        })
+    }
+}
+
 }
 
 #[derive(Clone, Debug)]
@@ -39,5 +61,20 @@ impl FromStr for Symbol {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Symbol(s.to_string()))
+    }
+}
+
+pub struct Wallet {
+    pub base: f64,
+    pub quote: f64,
+}
+
+impl Wallet {
+    pub fn init(initial_amount: f64, price: f64) -> Wallet {
+        let quote = initial_amount / 2.0;
+        Self {
+            base: quote / price,
+            quote,
+        }
     }
 }
